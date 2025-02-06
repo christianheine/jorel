8984467c951120875009adfbd8cdba4a592e3293
Christian Heine <christianheine@icloud.com>
2025-02-06 17:59:08 +1100

    Revert weather api example to use an actual API

--- examples/_utilities/get-weather.ts
+++ examples/_utilities/get-weather.ts
@@ -1,17 +1,12 @@
 export const getWeather = async ({ city }: { city: string }) => {
-  return {
-    city,
-    temperature: 25,
-    condition: "Sunny",
-  };
   // Request weather data from the Weather API
   // Requires a Weather API key
-  // const apiKey = process.env.WEATHER_API_KEY;
-  // const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`);
-  // const data = await response.json();
-  // return {
-  //   city: data.location.name,
-  //   temperature: data.current.temp_c,
-  //   condition: data.current.condition.text,
-  // };
+  const apiKey = process.env.WEATHER_API_KEY;
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`);
+  const data = await response.json();
+  return {
+    city: data.location.name,
+    temperature: data.current.temp_c,
+    condition: data.current.condition.text,
+  };
 };
