bb85055c39d8d3a252bebec875bead15937d6e73
Christian Heine <christianheine@icloud.com>
2025-02-27 14:20:13 +1100

    Update Anthropic default models

--- src/providers/anthropic/default-models.ts
+++ src/providers/anthropic/default-models.ts
@@ -1,10 +1,15 @@
 export const defaultAnthropicModels: string[] = [
+  "claude-3-5-haiku-latest",
   "claude-3-5-haiku-20241022",
+  "claude-3-7-sonnet-latest",
+  "claude-3-7-sonnet-20250219",
+  "claude-3-5-sonnet-latest",
   "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-20240620",
-  "claude-3-haiku-20240307",
-  "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
+  "claude-3-opus-latest",
+  "claude-3-sonnet-20240229",
+  "claude-3-haiku-20240307",
   "claude-2.1",
   "claude-2.0",
 ];
